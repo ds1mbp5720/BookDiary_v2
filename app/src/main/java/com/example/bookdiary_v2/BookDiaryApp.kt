046be4b59df8dc78928918323dec5a6cf047da1e
JungYoonLee <37658906+ds1mbp5720@users.noreply.github.com>
2024-01-16 15:19:36 +0900

    프로젝트 생성 및 초기 세팅

---
+++ app/src/main/java/com/example/bookdiary_v2/BookDiaryApp.kt
@@ -0,0 +1,9 @@
+package com.example.bookdiary_v2
+
+import android.app.Application
+
+class BookDiaryApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ presentation/build.gradle.kts
@@ -0,0 +1,87 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.mylibrary"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // androidx ui components
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.material:material:1.11.0")
+
+    val composeBom = platform("androidx.compose:compose-bom:2023.06.01")
+    implementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.compose.foundation:foundation-android:1.5.2")
+    implementation("androidx.navigation:navigation-compose:2.7.3")  // 2.4.2
+
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    kapt("com.github.bumptech.glide:compiler:4.11.0")
+    implementation("com.github.bumptech.glide:compose:1.0.0-alpha.1")
+
+    //ImagePicker
+    implementation("com.github.Drjacky:ImagePicker:2.1.12")
+//EventBus
+    implementation("com.squareup:otto:1.3.7")
+    implementation("org.greenrobot:eventbus:3.2.0")
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.46")
+    kapt("com.google.dagger:hilt-compiler:2.46")
+}
